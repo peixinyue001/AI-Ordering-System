226c8ea019d48bf581ad9f23e6f7abf1fb92ceed
peixinyue001 <peixinyue001@gmail.com>
2026-05-21 18:56:27 +0800

    同步配置文件

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-PINECONE_API_KEY = 'pcsk_2qfffC_4HnGAhGhZ14ckmpkR4yRKJrQr5Ee4RXhMEuC51dVJWP8Rk21JFcaR2fe21sdm4W'
 PINECONE_ENV = "us-east-1"
-DASHSCOPE_API_KEY= 'sk-025fe0b1293d4785b29ab4fa09b49379'
 DASHSCOPE_API_BASE = 'https://dashscope.aliyuncs.com/compatible-mode/v1/'
 DASHSCOPE_MODEL_NAME = 'qwen2.5-14b-instruct'
 
